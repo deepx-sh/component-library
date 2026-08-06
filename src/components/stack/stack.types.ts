06cfff0d1c364c49f69fdfd27196e9d32412f4bb
Deep Prajapati <dvprajapati7212@gmail.com>
2026-08-06 16:22:06 +0530

    feat: add type definitions for stack component

---
+++ src/components/stack/stack.types.ts
@@ -0,0 +1,31 @@
+import type { ReactNode } from "react";
+
+export interface CardRotateProps{
+    children: ReactNode;
+    onSendToBack: () => void;
+    sensitivity: number;
+    disableDrag?:false
+}
+
+export interface AnimationConfig{
+    stiffness: number;
+    damping: number;
+}
+
+export interface StackProps{
+    randomRotation?: boolean;
+    sensitivity?: number;
+    sendToBackOnClick?: boolean;
+    card?: ReactNode[];
+    animationConfig?: AnimationConfig;
+    autoplay?: boolean;
+    autoplayDelay?: number;
+    pauseOnHover?: boolean;
+    mobileClickOnly?: boolean;
+    mobileBreakpoint?: number;
+}
+
+export interface CardItem{
+    id: number;
+    content: ReactNode;
+}
