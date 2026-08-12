8d8d6aca218c4a14a520296987672f8248c92009
Deep Prajapati <dvprajapati7212@gmail.com>
2026-08-12 21:32:09 +0530

    feat: update index file to export TiltedCard component and its types

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,5 @@
 export { default as Stack } from './stack/Stack.tsx'
 export * from './stack/stack.types.ts'
+
+export { default as TiltedCard } from './tilted-card/TiltedCard.tsx'
+export * from './tilted-card/tilted-card.types.ts'
