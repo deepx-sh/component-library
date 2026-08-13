920ef19f59a9ed9d52a00e8b242dfc192bf611fc
Deep Prajapati <dvprajapati7212@gmail.com>
2026-08-13 21:15:03 +0530

    feat: add StarBorder component with customizable properties and animations

---
+++ src/components/star-border/StarBorder.tsx
@@ -0,0 +1,48 @@
+import type { ComponentPropsWithoutRef, ElementType } from "react";
+import type { StarBorderProps } from "./star-border.types";
+
+export default function StarBorder<T extends ElementType = 'button'>({
+    as,
+    className = '',
+    color = 'white',
+    speed = '6s',
+    thickness = 1,
+    children,
+    ...props
+    
+}: StarBorderProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof StarBorderProps<T>>) {
+    const Component = as || 'button';
+
+    return (
+        <Component
+            className={`relative inline-block py-px px-px overflow-hidden rounded-[20px] ${className}`}
+            {...props}
+        >
+            <div
+                className="absolute w-[300%] h-[50%] opacity-70 -bottom-2.75 right-[-250%] rounded-full animate-star-movement-bottom z-0"
+                style={{
+                    background: `radial-gradient(circle,${color} 0%,transparent 10%),radia-gradient(circle,${color} 10%,transparent 20%),radial-gradient(circle,${color} 20%,transparent 40%)`,
+                    animationDuration:speed
+                }}
+            />
+            
+            <div
+                className="absolute w-[300%] h-[50%] opacity-70 -top-2.5 left-[-250%] rounded-full animate-star-movemet-top z-0"
+                style={{
+                    background: `radial-gradient(circle,${color} 0%,transparent 10%),radia-gradient(circle,${color} 10%,transparent 20%),radial-gradient(circle,${color} 20%,transparent 40%)`,
+                    animationDuration:speed
+                }}
+            />
+
+            <div
+                className="relative z-10 bg-slate-900 border border-slate-800 text-white text-center rouded-[20px]"
+                style={{
+                    padding: '12px 24px',
+                    borderWidth:`${thickness}px`
+                }}
+            >
+                {children}
+            </div>
+        </Component>
+    )
+}
