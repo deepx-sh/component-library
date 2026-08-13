322e754c4205e0bcdc9005b40ba3e2d3dd68af1f
Deep Prajapati <dvprajapati7212@gmail.com>
2026-08-13 21:15:37 +0530

    feat: update index file to include StarBorder component and its types

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,7 @@ export * from './stack/stack.types.ts'
 
 export { default as TiltedCard } from './tilted-card/TiltedCard.tsx'
 export * from './tilted-card/tilted-card.types.ts'
+
+
+export { default as StarBorder } from './star-border/StarBorder.tsx'
+export *from './star-border/star-border.types.ts'
