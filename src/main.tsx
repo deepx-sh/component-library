dfa3aed10ea0a6ec6d9459cf0fde816a98c806da
Deep Prajapati <dvprajapati7212@gmail.com>
2026-08-29 22:33:24 +0530

    feat: integrate ClickSpark component into main application render

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import ClickSpark from './components/click-spark/ClickSpark.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ClickSpark
+      sparkColor='#ffffff'
+      sparkSize={10}
+      sparkRadius={15}
+      sparkCount={8}
+      duration={400}
+    >
+      <App />
+    </ClickSpark>
   </StrictMode>,
 )
